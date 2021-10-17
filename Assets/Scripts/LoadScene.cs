32b3fb306c212e68184dbdb2bc7b38a03bd6f669
lichadehehehe <chuanxu.richard.wang@gmail.com>
2021-10-17 16:23:50 +1100

    fixed a lot of gui bugs, exit button not yet complete

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -6,10 +6,19 @@ using UnityEngine.UI;
 
 public class LoadScene : MonoBehaviour
 {
+
+    public RectTransform canvasScreenRect;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+        canvasScreenRect.sizeDelta = new Vector2(Screen.width, Screen.height);
+        canvasScreenRect.anchoredPosition = new Vector2(0f, 0f);
+
+
     }
 
     // Update is called once per frame
@@ -22,6 +31,14 @@ public class LoadScene : MonoBehaviour
     {
         SceneManager.LoadSceneAsync("SampleScene", LoadSceneMode.Single);
         
+
+
+    }
+
+    public void QuitGame()
+    {
+
+        SceneManager.LoadSceneAsync("StartScene", LoadSceneMode.Single);
 
 
 
