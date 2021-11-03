33bfc4394205dd153301787515ec920c5560e5fc
lichadehehehe <chuanxu.richard.wang@gmail.com>
2021-11-03 21:45:40 +1100

    simple level 2 typing game implemented
    beta level 2

---
+++ Assets/Scripts/Typer.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Typer : MonoBehaviour
+{
+    
+    public Text wordOutput = null;
+
+    private string remainingWord = string.Empty;
+    private string currentWord = "testing";
+
+    void Start()
+    {
+        SetCurrentWord();
+    }
+
+
+    private void SetCurrentWord()
+    {
+        SetRemainingWord(currentWord);
+
+    }
+
+    private void SetRemainingWord(string newString)
+    
+    {
+        remainingWord = newString;
+        wordOutput.text = remainingWord;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+        CheckInput();
+        
+    }
+
+    void CheckInput()
+    {
+
+        if (Input.anyKeyDown)
+        {
+            string keysPressed = Input.inputString;
+
+            if (keysPressed.Length == 1)
+            {
+                EnterLetter(keysPressed);
+
+
+
+            }
+
+
+        }
+
+
+    }
+
+    void EnterLetter(string typedLetter)
+    {
+        if (IsCorrectLetter(typedLetter))
+        
+        {
+
+            Removeletter();
+
+            if (IsWordComplete())
+            
+            {
+                SetCurrentWord();
+
+            }
+
+        }
+
+
+
+    }
+
+    bool IsCorrectLetter(string letter)
+
+    {
+        
+        
+        return remainingWord.IndexOf(letter) == 0;
+        
+        
+    }
+
+
+
+    void Removeletter()
+    {
+
+        string newString = remainingWord.Remove(0,1);
+        SetRemainingWord(newString);
+
+    }
+
+    bool IsWordComplete()
+    {
+
+        return remainingWord.Length == 0;
+
+
+    }
+
+
+}
